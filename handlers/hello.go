2d534d0963ef0f3f8cbab4f6c873d2877b6dd675
Daniel Gerlach <wrack1981@gmx.de>
2016-09-24 16:54:24 +0200

    v0.2.0 completed

--- handlers/hello.go
+++ handlers/hello.go
@@ -14,7 +14,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	response := HelloResponse{
 		Message: "hello world",
 	}
-	log.Printf("Sending hello to the world...")
+	log.Printf("/hello requested...")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 	return
