aa76262a5916c677ba4a7d1a59c4a55cc02edc20
Daniel Gerlach <wrack1981@gmx.de>
2016-10-06 19:08:16 +0200

    added graceful shutdonw httpServer, dynamic portbinding with NOMAD_ADDR_http and it's v0.1.0 again

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ ADD . /go/src/github.com/gerlacdt/hello
 WORKDIR /go/src/github.com/gerlacdt/hello
 
 # build binary
-RUN go build -o main
-
-EXPOSE 3000
+RUN go get && go build -o main
 
 CMD ["./main"]
+
+# EXPOSE not needed because of dynamic portbinding with nomad
--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/braintree/manners"
 	"github.com/gerlacdt/hello/handlers"
 	"log"
 	"net/http"
@@ -18,21 +20,46 @@ func logEnvionmentVariables() {
 func main() {
 	log.Println("Starting hello-app...")
 
-	// handle SIGTERM
-	c := make(chan os.Signal)
+	logEnvionmentVariables()
 
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		log.Fatalf("Cleaning up before killing process...")
-	}()
+	httpAddr := os.Getenv("NOMAD_ADDR_http")
+	if httpAddr == "" {
+		log.Fatal("NOMAD_ADDR_http must be set!")
+	}
 
 	// create web-router
 	mux := http.NewServeMux()
 	mux.Handle("/hello", http.HandlerFunc(handlers.HelloHandler))
+	mux.Handle("/hello/version", http.HandlerFunc(handlers.VersionHandler))
+	mux.Handle("/hello/health", http.HandlerFunc(handlers.HealthHandler))
 	mux.Handle("/version", http.HandlerFunc(handlers.VersionHandler))
 	mux.Handle("/health", http.HandlerFunc(handlers.HealthHandler))
 
 	// start server
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	httpServer := manners.NewServer()
+	httpServer.Addr = httpAddr
+	httpServer.Handler = mux
+
+	// handle graceful shutdown
+	errChan := make(chan error, 10)
+
+	go func() {
+		errChan <- httpServer.ListenAndServe()
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case err := <-errChan:
+			if err != nil {
+				log.Fatal(err)
+			}
+		case s := <-signalChan:
+			log.Println(fmt.Sprintf("Caputured %v. Exiting...", s))
+			httpServer.BlockingClose()
+			os.Exit(0)
+		}
+	}
 }
